fbf98eaf0c266f5eada2d249932a3a307369dc93
RaulBelmonteSouza <raulbelmontedesouza@gmail.com>
2023-10-19 09:21:20 -0300

    Configuring custom message source for validations

---
+++ src/main/java/com/raulbsouza/wex/TransactionSolution/configuration/CustomMessageSourceConfiguration.java
@@ -0,0 +1,28 @@
+package com.raulbsouza.wex.TransactionSolution.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class CustomMessageSourceConfiguration {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+}
