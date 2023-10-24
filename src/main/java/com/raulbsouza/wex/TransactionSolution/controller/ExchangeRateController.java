45ccb5fc17ec68af33a5e01ca679831f8d2d3037
RaulBelmonteSouza <raulbelmontedesouza@gmail.com>
2023-10-24 02:28:37 -0300

    Documenting ExchangeRate operations

--- src/main/java/com/raulbsouza/wex/TransactionSolution/controller/ExchangeRateController.java
+++ src/main/java/com/raulbsouza/wex/TransactionSolution/controller/ExchangeRateController.java
@@ -2,7 +2,10 @@ package com.raulbsouza.wex.TransactionSolution.controller;
 
 import com.raulbsouza.wex.TransactionSolution.dto.ExchangeRateDataDTO;
 import com.raulbsouza.wex.TransactionSolution.service.ExchangeRateService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,17 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDate;
 
 @RestController
-@RequestMapping("exchange-rates")
+@RequestMapping(value = "exchange-rates", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ExchangeRateController {
 
     @Autowired
     private ExchangeRateService exchangeRateService;
 
+    @Operation(summary = "List all valid country currency for exchange rate.",
+            description = "Null fields in the response will be ignored.")
     @GetMapping("/countries")
     public ExchangeRateDataDTO get() {
         return exchangeRateService.retrieveExchangeRateDate();
     }
 
+    @Operation(summary = "Get exchange information for specified currency from the date informed " +
+            "within the last 6 months.", description = "Null fields in the response will be ignored.")
+    @ApiResponse(responseCode = "200")
     @GetMapping
     public ExchangeRateDataDTO getExchangeForCurrency(@RequestParam String currency, @RequestParam LocalDate date) {
         return exchangeRateService.retrieveExchangeForCurrency(currency, date);
