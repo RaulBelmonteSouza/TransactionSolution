442e2682abd571a8865c9ca75cb641089d1d1c6c
RaulBelmonteSouza <raulbelmontedesouza@gmail.com>
2023-10-23 21:34:49 -0300

    Creating endpoint to use transaction currency conversion service

--- src/main/java/com/raulbsouza/wex/TransactionSolution/controller/TransactionController.java
+++ src/main/java/com/raulbsouza/wex/TransactionSolution/controller/TransactionController.java
@@ -1,14 +1,20 @@
 package com.raulbsouza.wex.TransactionSolution.controller;
 
 import com.raulbsouza.wex.TransactionSolution.dto.TransactionDTO;
+import com.raulbsouza.wex.TransactionSolution.dto.TransactionWithCurrencyConvertedDTO;
+import com.raulbsouza.wex.TransactionSolution.exception.ExchangeRateClientException;
+import com.raulbsouza.wex.TransactionSolution.exception.ResourceNotFoundException;
 import com.raulbsouza.wex.TransactionSolution.searchcriteria.TransactionSearchCriteria;
 import com.raulbsouza.wex.TransactionSolution.service.TransactionService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/transactions")
@@ -27,6 +33,14 @@ public class TransactionController {
                                        @Validated TransactionSearchCriteria criteria) {
         return transactionService.filterAndPage(criteria, pageable)
                 .map(transaction -> new TransactionDTO().toDTO(transaction));
+    }
+
+    @GetMapping("/{id}")
+    public TransactionWithCurrencyConvertedDTO getByIdAndCurrency
+            (@PathVariable UUID id, @PathParam("currency") String currency)
+            throws ResourceNotFoundException, ExchangeRateClientException {
+
+        return transactionService.getTransactionByIdAndCurrency(id, currency);
     }
 
 }
