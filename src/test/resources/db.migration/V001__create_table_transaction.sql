91e8ecd9ea375bbeb775e7832fef0382fdda2c4c
RaulBelmonteSouza <raulbelmontedesouza@gmail.com>
2023-10-26 15:19:25 -0300

    Fixing test migrations path

--- src/test/resources/db.migration/V001__create_table_transaction.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS "transaction";
-
-CREATE TABLE "transaction" (
-                               "transaction_id" uuid DEFAULT random_uuid(),
-                               "description" VARCHAR(50) NOT NULL,
-                               "transaction_date" DATE NOT NULL,
-                               "amount" NUMERIC NOT NULL,
-                               PRIMARY KEY ("transaction_id")
-)
